fea62f79376448394a5f44692986c03d3e1535c8
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2022-11-07 10:33:36 +0000

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; Affordabu</Col>
+          <Col className="text-center py-3">Copyright &copy; Kingmuffy</Col>
         </Row>
       </Container>
     </footer>
